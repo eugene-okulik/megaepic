2b218d74cde3626b72e1b86d65eff9ef36365b0f
Elbey Gamidov <megaepicccc@gmail.com>
2024-02-16 16:04:36 +0600

    New PY file

---
+++ homework/Elbey_gamidov/Lesson_3/Lesson3Code.py
@@ -0,0 +1,26 @@
+from math import sqrt
+
+a = int(input('Введи первое число: '))
+b = int(input('Введи второе число: '))
+print(a + b)
+print(a - b)
+print(a * b)
+ 
+
+x = int(input('Введи первое число: '))
+y = int(input('Введи второе число: '))
+print(((x - y)/1) + (x * y))
+   
+
+a = int(input('Введи первое число: '))
+b = int(input('Введи второе число: '))
+print((a + b)/2)
+print(sqrt(a * b))
+   
+   
+a = int(input('Введи первый катет: '))
+b = int(input('Введи второй катет: '))
+gypo = sqrt(((a * a) + (b * b)))
+s = 0.5 * (a * b)
+print(gypo)
+print(s)
