179a08e5e3d0a276f75cd60458d03d40d563f6ca
Elbey Gamidov <megaepicccc@gmail.com>
2024-02-20 04:22:55 +0600

    Lesson_5

--- homework/Elbey_gamidov/Lesson_5/Lesson_5.py
+++ homework/Elbey_gamidov/Lesson_5/Lesson_5.py
@@ -1,5 +1,5 @@
 person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
-name , last_name, city, phone, country = person
+name, last_name, city, phone, country = person
 
 
 text_1 = 'результат операции: 42'
