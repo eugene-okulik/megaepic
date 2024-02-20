bcffe2be0fa3df8cb5be9c5300947c5229ce338c
Elbey Gamidov <megaepicccc@gmail.com>
2024-02-20 16:09:20 +0600

    Lesson_5_v2

--- homework/Elbey_gamidov/Lesson_5/Lesson_5.py
+++ homework/Elbey_gamidov/Lesson_5/Lesson_5.py
@@ -5,12 +5,15 @@ name, last_name, city, phone, country = person
 text_1 = 'результат операции: 42'
 text_2 = 'результат операции: 514'
 text_3 = 'результат работы программы: 9'
-print(text_1.index('42'))
-print(text_2.index('514'))
-print(text_3.index('9'))
-print(int(text_1[20:22]) + 10)
-print(int(text_2[20:23]) + 10)
-print(int(text_3[-1:]) + 10)
+id = text_1.find(': ') # нашел индекс этого двоеточия
+# print(id)
+print(int(text_1[20:]) + 10) # прибавил к верхнему индексу два значения(зная, что двоеточие и пробел перед цифрой), и сказал, что выведи все до конца + 10
+id_2 = text_2.find(': ')
+# print(id_2)
+print(int(text_2[20:]) + 10) # Тут все тоже самое
+id_3 = text_3.find(': ')
+# print(id_3)
+print(int(text_3[28:]) + 10)
 
 
 students = ['Ivanov', 'Petrov', 'Sidorov']
