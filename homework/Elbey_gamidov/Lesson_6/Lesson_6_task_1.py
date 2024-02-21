588c0efc3da38f0c899026384425e12ebd544149
Elbey Gamidov <megaepicccc@gmail.com>
2024-02-22 05:38:06 +0600

    Lesson_6_both_tasks

--- homework/Elbey_gamidov/Lesson_6/Lesson_6_task_1.py
+++ homework/Elbey_gamidov/Lesson_6/Lesson_6_task_1.py
@@ -1,14 +1,14 @@
 text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
 
-words = text.split()  # Перевел все в массив  
+words = text.split()  # Перевел все в массив 
 fin_words = []  # Завел новый массив
 for word in words:  # Иду по циклу
     if word.endswith(','):  # Если слово заканчивается на запятую
-        new_word = word.replace(',' , '')  # то меняю запятую на ничто
+        new_word = word.replace(',', '')  # то меняю запятую на ничто
         fin_words.append(new_word)  # пушу слово в новый массив
     elif word.endswith('.'):  # Тут тоже самое, но с точкой
-        second_new_word = word.replace('.' , '')
-        fin_words.append(second_new_word)        
+        second_new_word = word.replace('.', '')
+        fin_words.append(second_new_word)       
     else:
         fin_words.append(word)
 print('ing '.join(fin_words))  # добавляю ing к каждому слову
