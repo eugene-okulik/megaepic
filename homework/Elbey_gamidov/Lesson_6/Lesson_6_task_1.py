f62ffd1563b65e8631eb5c2cc2298122eb4a2e89
Elbey Gamidov <megaepicccc@gmail.com>
2024-02-22 05:36:47 +0600

    Lesson_6_both_tasks

--- homework/Elbey_gamidov/Lesson_6/Lesson_6_task_1.py
+++ homework/Elbey_gamidov/Lesson_6/Lesson_6_task_1.py
@@ -4,10 +4,10 @@ words = text.split()  # Перевел все в массив
 fin_words = []  # Завел новый массив
 for word in words:  # Иду по циклу
     if word.endswith(','):  # Если слово заканчивается на запятую
-        new_word = word.replace(',', '')  # то меняю запятую на ничто
+        new_word = word.replace(',' , '')  # то меняю запятую на ничто
         fin_words.append(new_word)  # пушу слово в новый массив
     elif word.endswith('.'):  # Тут тоже самое, но с точкой
-        second_new_word = word.replace('.', '')
+        second_new_word = word.replace('.' , '')
         fin_words.append(second_new_word)        
     else:
         fin_words.append(word)
