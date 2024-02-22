ddd6c047096289f3300b788727c2151fa274a526
Elbey Gamidov <megaepicccc@gmail.com>
2024-02-22 17:34:44 +0600

    Lesson_6_both_tasks_v1.1

--- homework/Elbey_gamidov/Lesson_6/Lesson_6_task_2.py
+++ homework/Elbey_gamidov/Lesson_6/Lesson_6_task_2.py
@@ -1,10 +1,10 @@
 diapazon = range(1, 101)
 for i in diapazon:
-    if i % 3 == 0:
-        print('Fuzz')
-    if i % 5 == 0:
-        print('Buzz')
-    elif i % 3 and i % 5 == 0:
+    if i % 3 == 0 and i % 5 == 0:
         print('FuzzBuzz')
+    elif i % 3 == 0:
+        print('Fuzz')
+    elif i % 5 == 0:
+        print('Buzz')
     else:
         print(i)
