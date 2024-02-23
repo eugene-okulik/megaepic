cc26255466a338ae96c3e00c43416aa43fdd5504
Elbey Gamidov <megaepicccc@gmail.com>
2024-02-23 16:56:33 +0600

    Lesson_6_both_tasks_v2.0

--- homework/Elbey_gamidov/Lesson_6/Lesson_6_task_1.py
+++ homework/Elbey_gamidov/Lesson_6/Lesson_6_task_1.py
@@ -1,15 +1,15 @@
 text = '''Etiam tincidunt neque erat, quis molestie enim imperdiet vel.
 Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'''  # Правильно перенес строку? а то линт ругается
 
-words = text.split()  # Перевел все в массив
-fin_words = []  # Завел новый массив
-for word in words:  # Иду по циклу
-    if word.endswith(','):  # Если слово заканчивается на запятую
-        new_word = word.replace(',', '')  # то меняю запятую на ничто
-        fin_words.append(new_word)  # пушу слово в новый массив
-    elif word.endswith('.'):  # Тут тоже самое, но с точкой
+words = text.split()
+fin_words = []
+for word in words:
+    if word.endswith(','):
+        new_word = word.replace(',', '')
+        fin_words.append('ing' + str(new_word) + str(','))
+    elif word.endswith('.'):
         second_new_word = word.replace('.', '')
-        fin_words.append(second_new_word)
+        fin_words.append('ing' + str(second_new_word) + str('.'))
     else:
-        fin_words.append(word)
-print('ing '.join(fin_words))  # добавляю ing к каждому слову
+        fin_words.append('ing' + str(word))
+print(' '.join(fin_words))
