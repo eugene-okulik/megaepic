08085264c73659651de88d7012bd0f13e8fa9ce3
Elbey Gamidov <megaepicccc@gmail.com>
2024-02-27 03:19:31 +0600

    Lesson_7_V_1.0

---
+++ homework/Elbey_gamidov/Lesson_7/Lesson_7_task_3.py
@@ -0,0 +1,15 @@
+text_1 = 'результат операции: 42'
+text_2 = 'результат операции: 514'
+text_3 = 'результат работы программы: 9'
+
+def text_find(text):  # Функция для первых двух переменных
+    id = text.find(': ')
+    print(int(text[20:]) + 10)
+
+def find_and_replace_3(text):  # Функция для третьей переменной
+    id_3 = text_3.find(': ')
+    print(int(text_3[28:]) + 10)
+
+text_find(text_1)
+text_find(text_2)
+find_and_replace_3(text_3)
