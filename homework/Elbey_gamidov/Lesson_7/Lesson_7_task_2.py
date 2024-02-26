0848df2d7053a46bd730dc6c59294feb8f0d23a6
Elbey Gamidov <megaepicccc@gmail.com>
2024-02-27 03:24:45 +0600

    Lesson_7_V_1.1

--- homework/Elbey_gamidov/Lesson_7/Lesson_7_task_2.py
+++ homework/Elbey_gamidov/Lesson_7/Lesson_7_task_2.py
@@ -1,9 +1,11 @@
 words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
 
+
 def how_much_times(dict):
     for key, value in words.items():
         for i in range(value):
             print(key, end='')
         print()
+
 
 how_much_times(words)
--- homework/Elbey_gamidov/Lesson_7/Lesson_7_task_3.py
+++ homework/Elbey_gamidov/Lesson_7/Lesson_7_task_3.py
@@ -3,12 +3,14 @@ text_2 = 'результат операции: 514'
 text_3 = 'результат работы программы: 9'
 
 def text_find(text):  # Функция для первых двух переменных
-    id = text.find(': ')
+    text.find(': ')
     print(int(text[20:]) + 10)
 
+
 def find_and_replace_3(text):  # Функция для третьей переменной
-    id_3 = text_3.find(': ')
+    text_3.find(': ')
     print(int(text_3[28:]) + 10)
+
 
 text_find(text_1)
 text_find(text_2)
