b58148a8d5c4e08966e03733653f0e29ad97581f
Elbey Gamidov <megaepicccc@gmail.com>
2024-02-27 15:20:49 +0600

    Lesson_7_V_1.3

--- homework/Elbey_gamidov/Lesson_7/Lesson_7_task_3.py
+++ homework/Elbey_gamidov/Lesson_7/Lesson_7_task_3.py
@@ -3,16 +3,13 @@ text_2 = 'результат операции: 514'
 text_3 = 'результат работы программы: 9'
 
 
-def text_find(text):  # Функция для первых двух переменных
-    text.find(': ')
-    print(int(text[20:]) + 10)
+def find_and_replace(text):
+    pos = text.find(': ')
+    if True:
+        value = int(text[pos + 2:]) + 10
+        print(value)
 
 
-def find_and_replace_3(text):  # Функция для третьей переменной
-    text_3.find(': ')
-    print(int(text_3[28:]) + 10)
-
-
-text_find(text_1)
-text_find(text_2)
-find_and_replace_3(text_3)
+find_and_replace(text_1)
+find_and_replace(text_2)
+find_and_replace(text_3)
