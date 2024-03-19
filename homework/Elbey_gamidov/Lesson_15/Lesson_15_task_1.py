a30fc56c8c6a45a72b1d9a6ac8e59e9e43e1a7a7
Elbey Gamidov <megaepicccc@gmail.com>
2024-03-19 22:21:09 +0600

    Lesson_15_V_1.0

--- homework/Elbey_gamidov/Lesson_15/Lesson_15_task_1.py
+++ homework/Elbey_gamidov/Lesson_15/Lesson_15_task_1.py
@@ -1,9 +1,9 @@
 import mysql.connector as mysql
 
 db = mysql.connect(
-    user='st-onl',
-    passwd='AVNS_tegPDkI5BlB2lW5eASC',
-    host='db-mysql-fra1-09136-do-user-7651996-0.b.db.ondigitalocean.com',
+    user='',
+    passwd='',
+    host='',
     port=25060,
     database='st-onl'
 )
