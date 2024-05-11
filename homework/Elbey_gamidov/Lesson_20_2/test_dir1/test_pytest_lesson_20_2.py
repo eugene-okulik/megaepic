77e6c24c37ff20b19933fc8fc278a5c0667e4655
Elbey Gamidov <megaepicccc@gmail.com>
2024-05-12 01:33:25 +0600

    Lesson_27_v_1

--- homework/Elbey_gamidov/Lesson_20_2/test_dir1/test_pytest_lesson_20_2.py
+++ homework/Elbey_gamidov/Lesson_20_2/test_dir1/test_pytest_lesson_20_2.py
@@ -35,7 +35,7 @@ def test_patch_one_post(get_object_id, before_all_tests_end):
         "name": "New name for test"
     }
     response = requests.patch(f'https://api.restful-api.dev/objects/{get_object_id}', json=body)
-    with allure.step(f'Check this name = New name for test'):
+    with allure.step('Check this name = New name for test'):
         assert response.json()['name'] == 'New name for test'
 
 
